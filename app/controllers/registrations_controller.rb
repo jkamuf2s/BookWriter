3f26651ced624b11a02945cd0e5067e585782a87
jkamuf2s <jochen.kamuf@smail.inf.h-brs.de>
2014-06-06 16:56:37 +0200

    implemented reCaptcha for devise/registration
    version 2: fixed that always all field are declared as missing even if only "Vorname" is missing

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,11 +3,9 @@ class RegistrationsController < Devise::RegistrationsController
 
   def create
     if verify_recaptcha
-      flash.delete :recaptcha_error
       super
     else
-      flash.delete :recaptcha_error
-      build_resource
+      build_resource(sign_up_params)
       resource.valid?
       resource.errors.add(:base, "There was an error with the recaptcha code below. Please re-enter the code.")
       clean_up_passwords(resource)
