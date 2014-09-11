15ca8a0762b87316c9cc175668cfded69f2819bf
avan2s <andreas.van@smail.inf.h-brs.de>
2014-09-11 20:02:50 +0200

    message text to textarea

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140718113515) do
+ActiveRecord::Schema.define(:version => 20140909200705) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
     t.integer  "edition"
     t.date     "published"
     t.string   "genre"
-    t.text     "abstract"
+    t.string   "abstract"
     t.string   "tags"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
@@ -125,12 +125,13 @@ ActiveRecord::Schema.define(:version => 20140718113515) do
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
   create_table "versions", :force => true do |t|
-    t.string   "item_type",  :null => false
-    t.integer  "item_id",    :null => false
-    t.string   "event",      :null => false
+    t.string   "item_type",      :null => false
+    t.integer  "item_id",        :null => false
+    t.string   "event",          :null => false
     t.string   "whodunnit"
     t.text     "object"
     t.datetime "created_at"
+    t.text     "object_changes"
   end
 
   add_index "versions", ["item_type", "item_id"], :name => "index_versions_on_item_type_and_item_id"
